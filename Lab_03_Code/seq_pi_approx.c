740bfcf2f805f338b2bbbf5e29b2e6bfdfdc40fe
denvas2024 <denvas_2000@yahoo.com>
2024-11-04 19:37:11 +0200

    Add files via upload

--- Lab_03_Code/seq_pi_approx.c
+++ Lab_03_Code/seq_pi_approx.c
@@ -9,15 +9,15 @@
 /*  Function pi_approx
     Calculates an approximation of pi value
 */    
-float pi_approx() {
+double pi_approx() {
 int i=0;
-float temp=0;
+double temp=0;
 
 do {
  i++;
  temp+=1.0/(i*i);
  printf("temp: %f, i: %d\n", temp, i);
-} while (i<200);  //end do .. while i
+} while (i<400);  //end do .. while i
 
 return sqrt(6*temp);
 } //end of function pi_approx
@@ -28,3 +28,27 @@ printf("The sum is: %f \n", pi_approx());
 return 0;
 }
 
+/*
+
+#include <stdio.h>
+#include <math.h>
+
+float pi_approx(int times) {
+int i=0;
+float temp=0;
+
+do {
+ i++;
+ temp+=1.0/(i*i);
+ printf("temp: %f, i: %d\n", temp, i);
+} while (i<times);  //end do .. while i
+
+return sqrt(6*temp);
+} //end of function pi_approx
+
+int main()
+{
+printf("The sum is: %f \n", pi_approx(200));
+return 0;
+}
+*/
