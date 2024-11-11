42b95a6446ea0c2ce342ba7bee74230628d8177b
denvas2024 <denvas_2000@yahoo.com>
2024-11-11 19:44:03 +0200

    Add files via upload

---
+++ Lab_04_Code/printchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+
+int i=0, rem;
+
+//for (int i=0;i++;i<=255) //In this we do not declare i in previous line
+
+for (i=0;i<=255;i++)
+{
+
+rem=i%3;
+
+if (rem==0 && i>=33 && i<=105) 
+    printf("%c - (dec: %d, hex: %x)\n", i, i, i);
+
+}
+
+}
