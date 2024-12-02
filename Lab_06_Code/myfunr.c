1a616a26b308799465831592ff4b2a1c94652b8b
denvas2024 <denvas_2000@yahoo.com>
2024-12-02 10:20:48 +0200

    Add files via upload

--- Lab_06_Code/myfunr.c
+++ Lab_06_Code/myfunr.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-void badf(int x, int y, int *sum, int *diff) {
+void goodf(int x, int y, int *sum, int *diff) {
 
 *sum = x + y;
 *diff = x - y;
-printf("The sum a+b is: %d, the difference a-b is: %d\n", *sum, *diff);
+//printf("The sum a+b is: %d, the difference a-b is: %d\n", sum, diff);
 
 }
+
 
 int main() {
 int sum=0, diff=0;    
 
-badf(2, 3, &sum, &diff); //Call by pointer
+goodf(2, 3, &sum, &diff); //Call by pointer
 printf("The sum a+b is: %d, the difference a-b is: %d\n", sum, diff);
 return 0;
 
